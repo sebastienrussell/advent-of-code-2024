e8baa20448b0904ec6a79869259271114c84bcb1
sebastienrussell <russell.sebas@gmail.com>
2024-12-01 10:52:47 -0500

    fixes
    Signed-off-by: sebastienrussell <russell.sebas@gmail.com>

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,15 @@
-import java.math.BigInteger
+import kotlin.math.absoluteValue
 
 fun main() {
-    fun parseLists(input: List<String>): Pair<List<BigInteger>, List<BigInteger>> {
-        val leftList = mutableListOf<BigInteger>()
-        val rightList = mutableListOf<BigInteger>()
+    fun parseLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        val leftList = mutableListOf<Int>()
+        val rightList = mutableListOf<Int>()
 
         // Parsed Lines
         input.map { line ->
             line.split("   ").let {
-                leftList.add(it[0].toBigInteger())
-                rightList.add(it[1].toBigInteger())
+                leftList.add(it[0].toInt())
+                rightList.add(it[1].toInt())
             }
         }
         leftList.sort()
@@ -17,39 +17,25 @@ fun main() {
         return Pair(leftList, rightList)
     }
 
-    fun calculateDistanceBetweenLists(leftList: List<BigInteger>, rightList: List<BigInteger>): List<BigInteger> =
-        leftList.mapIndexed { index, leftItem ->
-            val rightItem = rightList[index]
-            when (leftItem.compareTo(rightItem)) {
-                -1 -> rightItem - leftItem
-                1 -> leftItem - rightItem
-                else -> BigInteger.ZERO
-            }
-        }
-
-    fun calculateSimilaritiesBetweenLists(leftList: List<BigInteger>, rightList: List<BigInteger>): List<BigInteger> =
-        leftList.mapIndexed { index, leftItem ->
+    fun calculateSimilaritiesBetweenLists(leftList: List<Int>, rightList: List<Int>): List<Int> =
+        leftList.map { leftItem ->
             val rightItemsCount = rightList.count { it == leftItem }
-            leftItem * rightItemsCount.toBigInteger()
+            leftItem * rightItemsCount
         }
 
-    fun part1(input: List<String>): BigInteger =
-        parseLists(input).let { (leftList, rightList) ->
-            calculateDistanceBetweenLists(leftList, rightList)
-        }.sumOf { it }
+    fun part1(input: List<String>): Int =
+        parseLists(input).let { (leftList, rightList) -> leftList.zip(rightList) }
+            .sumOf { (x, y) -> (x - y).absoluteValue }
 
-    fun part2(input: List<String>): BigInteger =
+    fun part2(input: List<String>): Int =
         parseLists(input).let { (leftList, rightList) ->
             calculateSimilaritiesBetweenLists(leftList, rightList)
         }.sumOf { it }
 
-    // Test if implementation meets criteria from the description, like:
-//    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == BigInteger.valueOf(11))
-    check(part2(testInput) == BigInteger.valueOf(31))
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
